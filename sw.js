c8d186e36ffcbf5945628508253854261e5fe97f
Paolo Chang <chanhwan.chang@gmail.com>
2020-05-10 19:14:42 -0400

    Cache versioning

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v2';
 const assets = [
   '/',
   '/index.html',
@@ -9,6 +9,7 @@ const assets = [
   '/src/css/styles.css',
   '/src/assets/images/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
   '/src/app/pages/about.html',
   '/src/app/pages/contact.html'
 ];
@@ -22,18 +23,28 @@ self.addEventListener('install', event => {
       console.log('caching shell assets');
       cache.addAll(assets);
     })
-  )
+  );
 });
 
 // activate event
 self.addEventListener('activate', event => {
   // console.log('service worker has been activated');
-})
+
+  /** Cache versioning - delete old caches */
+  event.waitUntil(
+    caches.keys().then(keys => {
+      // console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      )
+    })
+  );
+});
 
 // fetch event
 self.addEventListener('fetch', event => {
   // console.log('fetch event', event);
-
   event.respondWith(
     caches.match(event.request).then(cacheRes => {
       return cacheRes || fetch(event.request);
